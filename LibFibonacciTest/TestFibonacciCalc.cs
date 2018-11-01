76207f55a0942cb0d89a803a8d193bdcb8595ca4
cbossie <cbossie@gmail.com>
2018-10-31 20:20:24 -0400

    Test

---
+++ LibFibonacciTest/TestFibonacciCalc.cs
@@ -0,0 +1,44 @@
+using System;
+using LibFibonacci;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibFibonacciTest
+{
+    [TestClass]
+    public class TestFibonacciCalc
+    {
+        [TestMethod]
+        public void TestFibonacciCalcBaseCase()
+        {
+            var i = 1;
+            var f = FiboonacciCalc.GetNthFibonacciString(i);
+            Assert.AreEqual(f, "1");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestFibonacciCalcNegative()
+        {
+            var i = -1;
+            var f = FiboonacciCalc.GetNthFibonacciString(i);
+        }
+
+        [TestMethod]
+        public void TestFibonacciCalcZero()
+        {
+            var i = 0;
+            var f = FiboonacciCalc.GetNthFibonacciString(i);
+            Assert.AreEqual(f, "0");
+        }
+
+
+        [TestMethod]
+        public void TestFibonacciCalcLarger()
+        {
+            var i = 44;
+            var f = FiboonacciCalc.GetNthFibonacciString(i);
+            Assert.AreEqual(f, "701408733 ");
+        }
+
+    }
+}
