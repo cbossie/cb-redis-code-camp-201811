0535aac117121882d21640eb2ba64485cd4ceaa3
cbossie <cbossie@gmail.com>
2018-11-12 20:57:43 -0500

    Add Subscriber

---
+++ ExampleRedisSubscriber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace ExampleRedisSubscriber
+{
+    class Program
+    {
+        private const string channel = "WEATHER";
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Subscribing to channel {channel}");
+
+            ConfigurationOptions redisOptions = new ConfigurationOptions()
+            {
+                EndPoints =
+                {
+                    {"127.0.0.1", 6379}
+                },
+                AbortOnConnectFail = false
+            };
+
+            using (var multiplexer = ConnectionMultiplexer.Connect(redisOptions))
+            {
+                PerformSubscription(multiplexer.GetSubscriber());
+                Console.WriteLine("Press Any Key to Continue...");
+                Console.ReadKey();
+            }
+        }
+
+        static void PerformSubscription(ISubscriber subscriber)
+        {
+            subscriber.Subscribe(channel, (redisChannel, value) =>
+            {
+                Console.WriteLine($"Message Received on channel {redisChannel}: \"{value}\"");
+
+            });
+        }
+    }
+}
